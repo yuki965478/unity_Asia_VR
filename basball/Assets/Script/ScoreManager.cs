29f3aa2f243801af5bb376cfaba3afacb7d2e35f
yuki965478 <71679934+yuki965478@users.noreply.github.com>
2020-12-22 12:00:07 +0800

    程式+布料

---
+++ basball/Assets/Script/ScoreManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    [Header("分數介面")]
+    public Text textScore;
+    [Header("分數")]
+    public int score;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print(other.name);
+    }
+}
